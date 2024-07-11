842e0421069578df15dcf151b57e7e7afd23a230
mantielero <josemaria.alkala@gmail.com>
2024-07-11 17:57:44 +0200

    in order to make it work with inlines

--- src/lvgl/lvgl.nim
+++ src/lvgl/lvgl.nim
@@ -1,2 +1,4 @@
-import wrapper/lvgl_9_1_1
-export lvgl_9_1_1
+{.push header:"/home/jose/src/nimlang/lvgl.nim/src/lvgl/submodules/lvgl/lvgl.h"}
+include wrapper/lvgl_9_1_1
+{.pop.}
+#export lvgl_9_1_1
