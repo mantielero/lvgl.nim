b45a526069565234244535e2a96f9366fbbfb624
mantielero <josemaria.alkala@gmail.com>
2024-07-11 18:06:17 +0200

    new API

--- examples/ex02_label.nim
+++ examples/ex02_label.nim
@@ -7,12 +7,16 @@ proc main =
   halInit(400,300) 
 
   # Change the active screen's background color
-  lv_obj_set_style_bg_color(lv_scr_act(), lv_color_hex(0x003a57'u32), LV_PART_MAIN.lv_style_selector_t)
+  lv_obj_set_style_bg_color(lv_screen_active(), 
+                            lv_color_hex(0x003a57'u32), 
+                            LV_PART_MAIN.lv_style_selector_t)
 
   # Create a white label, set its text and align it to the center
-  let label = lv_label_create(lv_scr_act())
+  let label = lv_label_create(lv_screen_active())
   label.lv_label_set_text("Hello world")
-  lv_obj_set_style_text_color(lv_scr_act(), lv_color_hex(0xffffff'u32), LV_PART_MAIN.lv_style_selector_t)
+  lv_obj_set_style_text_color(lv_screen_active(), 
+                              lv_color_hex(0xffffff'u32), 
+                              LV_PART_MAIN.lv_style_selector_t)
   lv_obj_align(label, LV_ALIGN_CENTER.lv_align_t, 0.lv_coord_t, 0.lv_coord_t)
 
   # Main loop
