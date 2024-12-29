8a69c44c0a34e7fa82bc4d25e81c5cec3364f71e
Jose Maria Garcia <josemaria.alkala@gmail.com>
2024-12-29 11:06:19 +0100

    v19.2 update

--- examples/ex16_styles_multiple.nim
+++ examples/ex16_styles_multiple.nim
@@ -21,8 +21,8 @@ proc main =
   lv_style_set_border_width(addr(style_base), 2)
   lv_style_set_radius(addr(style_base), 10)
   lv_style_set_shadow_width(addr(style_base), 10)
-  lv_style_set_shadow_ofs_y(addr(style_base), 5)
-  lv_style_set_shadow_opa(addr(style_base), LV_OPA_50)
+  lv_style_set_shadow_offset_y(addr(style_base), 5)
+  lv_style_set_shadow_opa(addr(style_base), LV_OPA_50.lv_opa_t)
   lv_style_set_text_color(addr(style_base), lv_color_white())
   lv_style_set_width(addr(style_base), 100)
 
@@ -39,14 +39,14 @@ proc main =
   lv_style_set_text_color(addr(style_warning),
                           lv_palette_darken(LV_PALETTE_YELLOW, 4))
   ## Create an object with the base style only
-  var obj_base: ptr lv_obj_t = lv_obj_create(lv_scr_act())
+  var obj_base: ptr lv_obj_t = lv_obj_create(lv_screen_active())
   lv_obj_add_style(obj_base, addr(style_base), 0)
   lv_obj_align(obj_base, LV_ALIGN_LEFT_MID, 20, 0)
   var label: ptr lv_obj_t = lv_label_create(obj_base)
   lv_label_set_text(label, "Base")
   lv_obj_center(label)
   ## Create another object with the base style and earnings style too
-  var obj_warning: ptr lv_obj_t = lv_obj_create(lv_scr_act())
+  var obj_warning: ptr lv_obj_t = lv_obj_create(lv_screen_active())
   lv_obj_add_style(obj_warning, addr(style_base), 0)
   lv_obj_add_style(obj_warning, addr(style_warning), 0)
   lv_obj_align(obj_warning, LV_ALIGN_RIGHT_MID, -20, 0)
